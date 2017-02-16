41ee82053e56ca024e7702244c28e96320f9a4b1
Jack <james.casebeer@gmail.com>
2017-02-16 18:52:58 -0500

    drawing a nice quad:)

--- main.c
+++ main.c
@@ -2,32 +2,48 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_opengl.h>
+#include <math.h>
+
+void draw_set_frustum(float fov, float ar,float znear, float zfar)
+{
+	const float DEG2RAD = 3.14159265/180;
+
+	float t = tan(fov/2 * DEG2RAD);
+	float height = znear * t;
+	float width = height * ar;
+
+	glFrustum(-width,width,-height,height,znear,zfar);
+}
 
 int main()
 {
-	SDL_Init(SDL_INIT_VIDEO);
-
 	SDL_Window *window = SDL_CreateWindow(
 		"Nebula",
 		SDL_WINDOWPOS_UNDEFINED,
 		SDL_WINDOWPOS_UNDEFINED,
 		640,360,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_FULLSCREEN_DESKTOP
+		SDL_WINDOW_OPENGL
 	);
 
 	SDL_Renderer *renderer = SDL_CreateRenderer(window,-1,0);
-	//SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY,"nearest");
-	//SDL_RenderSetLogicalSize(renderer,640,360);
-
-	SDL_GLContext glContext;
-	glContext = SDL_GL_CreateContext(window);
+	SDL_RenderSetLogicalSize(renderer,640,360);
 	
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER,1);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,2);
+
+	SDL_GL_CreateContext(window);
 
 	SDL_Event event;
 	int quit = 0;
+	unsigned int time = 0;
+	unsigned int timespent = 0;
+	#define TARGET_FPS 60
+	unsigned int sleeptime = 1000/TARGET_FPS;
 
 	while(!quit)
 	{
+		time = SDL_GetTicks();
+
 		while(SDL_PollEvent(&event))
 		{
 			if (event.type==SDL_QUIT)
@@ -36,10 +52,34 @@ int main()
 				if (event.key.keysym.sym == SDLK_ESCAPE)
 					quit = 1;
 		}
+
+		
+
 		glClearColor(0.50f,0.125f,0.25f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		draw_set_frustum(90,16/9,1,32000);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+
+		glBegin(GL_TRIANGLE_FAN);
+		glColor3f(1.f,0.f,0.f);
+		glVertex3f(0.f,0.f,0.f);
+		glColor3f(0.f,1.f,0.f);
+		glVertex3f(1.f,0.f,0.f);
+		glColor3f(0.f,0.f,1.f);
+		glVertex3f(1.f,1.f,0.f);
+		glColor3f(1.f,1.f,1.f);
+		glVertex3f(0.f,1.f,0.f);
+		glEnd();
+
 		SDL_GL_SwapWindow(window);
-		SDL_Delay(16);
+		
+		timespent = SDL_GetTicks() - time;
+		if (timespent<sleeptime)
+			SDL_Delay(sleeptime - timespent);
 	}
 
 	SDL_DestroyRenderer(renderer);

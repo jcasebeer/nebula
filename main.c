4fd725dac6a3d15f5656b7a3a7c4efc55f3a41df
Jack <james.casebeer@gmail.com>
2017-04-15 19:41:23 -0400

    compatibility profile

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ int main(int argc, char *argv[])
 	SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL,1);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION,3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION,0);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK,SDL_GL_CONTEXT_PROFILE_COMPATIBILITY);
 
 	// create our window
 	SDL_Window *window = SDL_CreateWindow(

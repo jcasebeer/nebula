1aa817cb12bbdad07aa101a567ad42013051a376
Jack <james.casebeer@gmail.com>
2018-03-14 20:43:24 -0400

    switched to 'unity' build system

---
+++ build.c
@@ -0,0 +1,14 @@
+#include "main.c"
+
+#include "entity.c"
+
+#include "game.c"
+
+#include "sound.c"
+
+#include "render.c"
+
+#include "gmath.c"
+
+#include "level.c"
+
---
+++ build.sh
@@ -0,0 +1,3 @@
+gcc -Wall -Wextra -std=c99 -O1 -msse2 -mfpmath=sse -IC:\SDL2\i686-w64-mingw32\include -c build.c
+gcc build.o -lmingw32 -mwindows -mconsole -LC:\SDL2\i686-w64-mingw32\lib -lSDL2main -lSDL2 -lopengl32 -lglu32 -lglew32 -lopenal32 -llibsndfile-1 -lm -o nebula.exe
+./nebula.exe
